54a93e7cc3f3b875dc4a57173d8a2cdbf0791128
Michael Egbinola <michaelmarketing25@gmail.com>
2023-05-04 20:07:16 -0400

    working on the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import "./footer.css";
+import LogoIcon from "../assets/icons/logo.svg";
+import FB from "../assets/icons/fb.png";
+import TI from "../assets/icons/tw.png";
+import YT from "../assets/icons/yt.png";
+import GooglePlus from "../assets/icons/g+.png";
+import LI from "../assets/icons/li.png";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <Box>
+      <Stack className="footer-section">
+        <Stack>
+          <img src={LogoIcon} alt="logo" width="50px" />
+          <Stack></Stack>
+        </Stack>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
